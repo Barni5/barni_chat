b7cc97ad1524b7f0c3d8ca19036168f768cec049
Barni5 <155485085+Barni5@users.noreply.github.com>
2024-04-05 16:13:22 +0200

    Update webhook_config.lua

--- [barni_chat]/barni_rpchat/configs/webhook_config.lua
+++ [barni_chat]/barni_rpchat/configs/webhook_config.lua
@@ -1,23 +1,23 @@
 Webhooks = { --add you own webhooks
-    ["looc"] = "https://discord.com/api/webhooks/1207070889322217533/5V7SrfuAE7au-1gHygCtQDScdVbPin_85TFzbLVB0kDU5XDILKMecR06IBXlBFBU0vWY",
+    ["looc"] = "https://discord.com/api/webhooks/link",
 
-    ["me"] = "https://discord.com/api/webhooks/1207070889322217533/5V7SrfuAE7au-1gHygCtQDScdVbPin_85TFzbLVB0kDU5XDILKMecR06IBXlBFBU0vWY",
+    ["me"] = "https://discord.com/api/webhooks/link",
 
-    ["do"] = "https://discord.com/api/webhooks/1207070889322217533/5V7SrfuAE7au-1gHygCtQDScdVbPin_85TFzbLVB0kDU5XDILKMecR06IBXlBFBU0vWY",
+    ["do"] = "https://discord.com/api/webhooks/link",
 
-    ["try"] = "https://discord.com/api/webhooks/1207070889322217533/5V7SrfuAE7au-1gHygCtQDScdVbPin_85TFzbLVB0kDU5XDILKMecR06IBXlBFBU0vWY",
+    ["try"] = "https://discord.com/api/webhooks/link",
 
-    ["blackmarket"] = "https://discord.com/api/webhooks/1207070889322217533/5V7SrfuAE7au-1gHygCtQDScdVbPin_85TFzbLVB0kDU5XDILKMecR06IBXlBFBU0vWY",
+    ["blackmarket"] = "https://discord.com/api/webhooks/link",
 
-    ["ad"] = "https://discord.com/api/webhooks/1207070889322217533/5V7SrfuAE7au-1gHygCtQDScdVbPin_85TFzbLVB0kDU5XDILKMecR06IBXlBFBU0vWY",
+    ["ad"] = "https://discord.com/api/webhooks/link",
 
-    ["staff"] = "https://discord.com/api/webhooks/1207070889322217533/5V7SrfuAE7au-1gHygCtQDScdVbPin_85TFzbLVB0kDU5XDILKMecR06IBXlBFBU0vWY",
+    ["staff"] = "https://discord.com/api/webhooks/link",
 
-    ["status"] = "https://discord.com/api/webhooks/1207070889322217533/5V7SrfuAE7au-1gHygCtQDScdVbPin_85TFzbLVB0kDU5XDILKMecR06IBXlBFBU0vWY",
+    ["status"] = "https://discord.com/api/webhooks/link",
 
-    ["here"] = "https://discord.com/api/webhooks/1207070889322217533/5V7SrfuAE7au-1gHygCtQDScdVbPin_85TFzbLVB0kDU5XDILKMecR06IBXlBFBU0vWY",
+    ["here"] = "https://discord.com/api/webhooks/link",
 
-    ["reports"] = "https://discord.com/api/webhooks/1207070889322217533/5V7SrfuAE7au-1gHygCtQDScdVbPin_85TFzbLVB0kDU5XDILKMecR06IBXlBFBU0vWY",
+    ["reports"] = "https://discord.com/api/webhooks/link",
 
-    ["directmessage"] = "https://discord.com/api/webhooks/1207070889322217533/5V7SrfuAE7au-1gHygCtQDScdVbPin_85TFzbLVB0kDU5XDILKMecR06IBXlBFBU0vWY",
+    ["directmessage"] = "https://discord.com/api/webhooks/link",
 }
